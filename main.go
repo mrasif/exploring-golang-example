6f0b0ba50ff099fa020d13722ecd56b7ed443196
Asif Mohammad Mollah <admin@mrasif.in>
2020-05-23 21:49:35 +0530

    chore: one value has many types

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
+type person struct {
+	first string
+}
+
+type human interface {
+	speak()
+}
+
+func (p person) speak() {
+	fmt.Println("from a person - this is my first name ",p.first)
+}
+
 func main() {
+	p1:= person{
+		first: "Asif",
+	}
+	fmt.Printf("%T\n",p1)
+
+	var x human
+	x=p1
+	fmt.Printf("%T\n",x)
 	
 }
