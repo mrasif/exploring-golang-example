cdf8f83aeb798ddd0288eec5e311857a7c205195
Asif Mohammad Mollah <admin@mrasif.in>
2020-05-23 22:14:57 +0530

    example 2

--- main.go
+++ main.go
@@ -2,26 +2,42 @@ package main
 
 import "fmt"
 
-type person struct {
-	first string
-}
-
 type human interface {
 	speak()
+}
+
+type person struct {
+	first string
 }
 
 func (p person) speak() {
 	fmt.Println("from a person - this is my first name ",p.first)
 }
 
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func (sa secretAgent) speak() {
+	fmt.Println("I am secret agent - this is my name ",sa.first)
+}
+
 func main() {
 	p1:= person{
 		first: "Asif",
 	}
-	fmt.Printf("%T\n",p1)
+	sa1 := secretAgent{
+		person: person{
+			first: "James",
+		},
+		ltk: true,
+	}
 
-	var x human
+	var x,y human
 	x=p1
-	fmt.Printf("%T\n",x)
+	y=sa1
+	x.speak()
+	y.speak()
 	
 }
